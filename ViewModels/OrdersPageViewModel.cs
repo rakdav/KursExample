72e54e727d11c834e14eeb5314d03f363b9876d0
rakdav <rakdav@inbox.ru>
2026-05-30 09:51:24 +0200

    OrdersPageViewModel

--- ViewModels/OrdersPageViewModel.cs
+++ ViewModels/OrdersPageViewModel.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using KursMVVM.Models;
+using KursMVVM.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace KursMVVM.ViewModels
 {
-    public class OrdersPageViewModel:ViewModelBase
+    public partial class OrdersPageViewModel:ViewModelBase
     {
-
+        private OrderPageService orderPageService;
+        [ObservableProperty]
+        private ObservableCollection<Order> orders = new();
+        [ObservableProperty]
+        private Order selectedOrder;
+        public OrdersPageViewModel()
+        {
+            orderPageService=new OrderPageService();
+        }
+        private void Load()
+        {
+            Orders.Clear();
+            Orders = new ObservableCollection<Order>(getOrders());
+        }
+        private List<Order> getOrders()
+        {
+            Task<List<Order>> task = Task.Run(() => orderPageService.getOrders());
+            return task.Result;
+        }
     }
 }
